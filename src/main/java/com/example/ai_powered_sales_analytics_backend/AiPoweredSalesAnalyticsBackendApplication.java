4f834f9539c35af9efa5f10c6a2fec267457a24e
salmanshaikssk007 <salmanshaikssk007@gmail.com>
2024-10-23 13:18:23 -0500

    Initial commit

---
+++ src/main/java/com/example/ai_powered_sales_analytics_backend/AiPoweredSalesAnalyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_powered_sales_analytics_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredSalesAnalyticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredSalesAnalyticsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_powered_sales_analytics_backend/AiPoweredSalesAnalyticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_powered_sales_analytics_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredSalesAnalyticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
